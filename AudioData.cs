3cca52f1bdd711a2513422f0820ac69a2506af51
LeafesMan <89503834+LeafesMan@users.noreply.github.com>
2024-08-23 23:43:23 -0500

    Rewrite
    - RandomAudioSets can randomize pitch, and volume in a range or via a list of potential values
    - Simplified by removing standard clips and variables clips and just keeping a RandomSpec generator along with AudioSpecs
    - No longer call LeafClip.Play() unclear what this does who manages it
    - Now call AudioManager.Play(AudioSpecs);
    - Clear that the audio manager handles the playing of the given AudioSpecs

---
+++ AudioData.cs
@@ -0,0 +1,19 @@
+/*
+ * Auth: Ian
+ * 
+ * Proj: SpaceS
+ * 
+ * Desc: Struct for
+ * 
+ * Date: 8/23/24
+ */
+using UnityEngine;
+using UnityEngine.Audio;
+
+public struct AudioData
+{
+    public AudioMixer mixer;
+    public AudioClip clip;
+    public float volume;
+    public float pitch;
+}
